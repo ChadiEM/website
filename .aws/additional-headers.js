844b12cc4f8ec6ec3c95bf0aa6ca217f8ca6b6ad
Chadi El Masri <chadimsr@gmail.com>
2025-11-03 23:16:28 +0100

    Add cloudfront function as part of the code

---
+++ .aws/additional-headers.js
@@ -0,0 +1,11 @@
+function handler(event) {
+  const response = event.response;
+  const headers = response.headers;
+
+  headers["strict-transport-security"] = { value: "max-age=63072000; includeSubDomains; preload" };
+  headers["content-security-policy"] = { value: "default-src 'none'; img-src 'self'; style-src 'self'; script-src 'self'; frame-ancestors 'none';" };
+  headers["x-content-type-options"] = { value: "nosniff" };
+  headers["cache-control"] = { value: "public, no-transform" };
+
+  return response;
+}
